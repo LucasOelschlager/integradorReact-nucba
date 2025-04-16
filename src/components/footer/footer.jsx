cbce4700ac888d3a3dfc6f2f4799278c2d14246a
Lucas Oelschlager <lucasoelschlager@gmail.com>
2025-04-16 18:32:47 -0300

    se inicio el footer, se añadio la categoria de all

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Footer = () => {
+    return (
+        <div>
+            <List>
+                <li>Direccion</li>
+                <li>Email</li>
+                <li>Telefono</li>
+                <li>Instagram</li>
+            </List>
+        </div>
+    )
+}
