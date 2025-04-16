e8a624b3cca0800731819edef9ef5a05540c5a43
Lucas Oelschlager <lucasoelschlager@gmail.com>
2025-04-16 19:28:00 -0300

    se hizo el footer

--- src/components/Categorys/Categorys.jsx
+++ src/components/Categorys/Categorys.jsx
@@ -3,16 +3,19 @@ import categorias from "../../data/categorys.json";
 
 export const Categorys = () => {
     return (
-        <div className={`${styledCategorys.container}`}>
-            <ul className={`${styledCategorys.list}`}>
-                {categorias.map((e) => (
-                    <li key={e.id} className={`${styledCategorys.categoryItem}`}>
-                        <img src={`${e.image}`} alt={e.name} className={`${styledCategorys.categoryImg}`} />
-                        <h4>{e.name}</h4>
-                        <p>{e.description}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <>
+            <h2 className="text-center text-4xl bg-[#0a0a0a]">Categorias</h2>
+            <div className={`${styledCategorys.container}`}>
+                <ul className={`${styledCategorys.list}`}>
+                    {categorias.map((e) => (
+                        <li key={e.id} className={`${styledCategorys.categoryItem}`}>
+                            <img src={`${e.image}`} alt={e.name} className={`${styledCategorys.categoryImg}`} />
+                            <h4>{e.name}</h4>
+                            <p>{e.description}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
     );
 };
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
+import { List } from '../List/List'
 
 export const Footer = () => {
     return (
-        <div>
-            <List>
-                <li>Direccion</li>
-                <li>Email</li>
-                <li>Telefono</li>
-                <li>Instagram</li>
-            </List>
-        </div>
+        <div className='w-[100%] flex '>
+            <List query="flex flex-col items-center justify-center bg-[#0a0a0a] w-[100%] gap-1">
+                <li className='text-2xl '>Direccion: <span className='font-serif text-[16px]'>Av. Alberdi 999</span></li>
+                <li className='text-2xl '>Email: <span className='font-serif text-[16px]'>rosarioguitarstore@gmail.com</span></li>
+                <li className='text-2xl'>Telefono: <span className='font-serif text-[16px]'>3444412312</span></li>
+                <li className='text-2xl'>Instagram: <span className='font-serif text-[16px]'>@rosarioguitarstore</span></li>
+                <li className='font-sans'>©Copyright Lucas Oelschlager</li>
+            </List >
+        </div >
     )
 }
